55c4019aa3d5aafbf65b81ac08601dcc83e9c6eb
Ontiretse Motlagale <OntiretseMotlagale@gmail.com>
2024-05-07 20:55:02 +0200

    Added he personal data on profileView

--- Crunch-App/Views/Profile/Views/SubViews/PersonalDataView.swift
+++ Crunch-App/Views/Profile/Views/SubViews/PersonalDataView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct PersonalDataView: View {
-    @State var fullname: String = ""
-    @State var email: String = ""
-    @State var dateOfBirth: String = ""
+    @State var fullname: String = "Bakang"
+    @State var email: String = "Bakang@gmail.com"
+    @State var showAlert: Bool = false
+    @State var newFullname: String = ""
+    @State var newEmail: String = ""
     var body: some View {
         ScrollView {
             VStack {
@@ -28,25 +30,90 @@ struct PersonalDataView: View {
                             .fill(.white)}
                     }
                     .padding(.bottom, 30)
-                VStack (alignment: .leading) {
-                    Text("Full Name")
-                        .foregroundStyle(Color("LightGray"))
-                        .padding(.bottom, 5)
-                    Text("Ontiretse Motlagale")
-                    Divider()
-                        .padding(.bottom, 5)
-                    Text("Email")
-                        .foregroundStyle(Color("LightGray"))
-                        .padding(.bottom, 5)
-                    Text("OntiretseMotlagale@gmail.com")
-                        .tint(.black)
-                    Divider()
+                buildAlert()
+                   
+                    .navigationTitle("Edit Profile")
+                CustomButton(title: "Save") {
+                    
                 }
             }
             .padding(.horizontal)
-            .navigationTitle("Edit Profile")
-            .navigationBarBackButtonHidden()
         }
+    }
+    
+    @ViewBuilder func buildUserProfileInputs() -> some View {
+        VStack  {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Full Name")
+                        .foregroundStyle(Color("LightGray"))
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 5)
+                    Text(fullname)
+                        .fontWeight(.semibold)
+                    Divider()
+                        .padding(.bottom, 5)
+                }
+                Button(action: {
+                    self.showAlert = true
+                    print("You tapped me")
+                }, label: {
+                    Text("Edit")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(AppColors.textColor)
+                })
+            }
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .foregroundStyle(Color("LightGray"))
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 5)
+                    Text(email)
+                        .tint(.black)
+                        .fontWeight(.semibold)
+                    Divider()
+                        .padding(.bottom, 5)
+                }
+                Button(action: {
+                    self.showAlert.toggle()
+                }, label: {
+                    Text("Edit")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(AppColors.textColor)
+                })
+            }
+        }
+    }
+    
+    @ViewBuilder func buildAlert() -> some View {
+        buildUserProfileInputs()
+            .alert("Edit Full Name", isPresented: $showAlert) {
+            TextField("Full Name", text: $newFullname)
+            Button(role: .cancel) {
+                fullname = newFullname
+            } label: {
+                Text("Save")
+            }
+            Button(role: .destructive) {
+                self.showAlert = false
+            } label: {
+                Text("Cancel")
+            }
+        }
+            .alert("Edit Email", isPresented: $showAlert) {
+                TextField("Email", text: $newEmail)
+                Button(role: .cancel) {
+                    email = newEmail
+                } label: {
+                    Text("Save")
+                }
+                Button(role: .destructive) {
+                    self.showAlert = false
+                } label: {
+                    Text("Cancel")
+                }
+            }
     }
 }
 
