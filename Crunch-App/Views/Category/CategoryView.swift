908b31108ba16ee957e5ebe1deb1a02bc1b0fcb3
Ontiretse Motlagale <OntiretseMotlagale@gmail.com>
2024-05-15 15:06:00 +0200

    Update

--- Crunch-App/Views/Category/CategoryView.swift
+++ Crunch-App/Views/Category/CategoryView.swift
@@ -12,30 +12,34 @@ struct CategoryView: View {
     var body: some View {
         VStack {
             VStack {
-                Image(category.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
+                VStack {
+                    Image(category.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                }
+                .frame(width: 150, height: 150)
+                .background(Color(category.color), in: RoundedRectangle(cornerRadius: 10))
+                .padding()
+                Spacer()
+                Text(category.name)
+                    .font(.headline)
+                    .padding(.bottom, 4)
+                    .foregroundStyle(.black)
+                Text("\(category.productModel.count) Items")
+                    .font(.footnote)
+                    .foregroundStyle(.black)
+                    .padding(6)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.2)))
+                    .padding(.bottom, 20)
             }
-            .frame(width: 150, height: 150)
-            .background(Color(category.color), in: RoundedRectangle(cornerRadius: 10))
-            .padding()
-            Spacer()
-            Text(category.name)
-                .font(.headline)
-                .padding(.bottom, 4)
-                .foregroundStyle(.black)
-            Text("\(category.productModel.count) Items")
-                .font(.footnote)
-                .foregroundStyle(.black)
-                .padding(6)
-                .background(RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.gray.opacity(0.2)))
-                .padding(.bottom, 20)
-        }
-        .frame(width: 170, height: 260)
-        .background(.white, in: RoundedRectangle(cornerRadius: 10))
+            .frame(width: 170, height: 260)
+            .background(.white, in: RoundedRectangle(cornerRadius: 10))
         .padding(.horizontal)
+        }
+        .shadow(color: .black.opacity(0.4), radius: 6, x: 0.5, y: 0.5)
+        
     }
 }
 
