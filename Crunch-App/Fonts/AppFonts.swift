18264ec69c89828d5f0542b1a20ac69543f6aab6
Ontiretse Motlagale <Ontiretse@glucode.com>
2024-05-31 13:10:48 +0200

    New custom fonts added

---
+++ Crunch-App/Fonts/AppFonts.swift
@@ -0,0 +1,15 @@
+//
+//  AppFonts.swift
+//  Crunch-App
+//
+//  Created by Ontiretse Motlagale on 2024/05/31.
+//
+
+import Foundation
+
+struct AppFonts {
+    static let regular = "Montserrat-Regular"
+    static let bold = "Montserrat-Bold"
+    static let black = "Montserrat-Black"
+    static let semibold = "Montserrat-SemiBold"
+}
